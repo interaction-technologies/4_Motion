c25380e0ce853b52f65d17b716b74b174a0328e1
George Cave <cave@hey.com>
2023-02-15 20:44:19 +0000

    Convert to tabs

--- Arduino_sketches/02_Wizard_of_Oz/02_Wizard_of_Oz.ino
+++ Arduino_sketches/02_Wizard_of_Oz/02_Wizard_of_Oz.ino
@@ -9,25 +9,25 @@ const int pot_pin = A3;
 
 void setup() {
 
-  Serial.begin(9600);
-  
-   // Attach the servo motor to the pin
-   motor.attach(motor_pin);
+	Serial.begin(9600);
+
+	// Attach the servo motor to the pin
+	motor.attach(motor_pin);
 }
 
 void loop() {
 
-  // Convert the analog position of potentiometer to a motor position
-  int pot_position = analogRead(pot_pin);
-  int motor_position = map(pot_position,0, 1023, 10, 170);
+	// Convert the analog position of potentiometer to a motor position
+	int pot_position = analogRead(pot_pin);
+	int motor_position = map(pot_position,0, 1023, 10, 170);
 
-  // Uncomment these lines if you want to see how the mapping works
-  // Serial.print(pot_position);
-  // Serial.print(" -> ");
-  // Serial.println(motor_position);
-  
-  // Move the motor!
-  motor.write(motor_position);
-  delay(50);
+	// Uncomment these lines if you want to see how the mapping works
+	// Serial.print(pot_position);
+	// Serial.print(" -> ");
+	// Serial.println(motor_position);
+
+	// Move the motor!
+	motor.write(motor_position);
+	delay(50);
 
 }
